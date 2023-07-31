331cfd663c3c16d8b77ef42b9656c5407c5d732f
henri <henri@PC-RICK>
2023-07-31 13:43:36 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Entre com um valor: ");
+double numero = Console.ReadLine();
+if (numero < 25.0)
+{
+    Console.WriteLine("O número digitado se encontra no intervalo de 0, 25");
+}
+else if (numero < 50.0)
+{
+    Console.WriteLine("O número digitado se encontra no intervalo de 25, 50");
+}
+else if (numero < 75.0)
+{
+    Console.WriteLine("O número digitado se encontra no intervalo de 25, 50");
+}
+else if (numero < 100.0)
+{
+    Console.WriteLine("O número digitado se encontra no intervalo de 25, 50");
+
+}
+else
