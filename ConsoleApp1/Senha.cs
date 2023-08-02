7eae55ce00ce0e5e278c6037622370da3b908a3c
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-02 15:13:00 -0300

    Exercicio com while referente a um login com senha e descobrir em qual quadrante os números estao

---
+++ ConsoleApp1/Senha.cs
@@ -0,0 +1,11 @@
+﻿int senha = 2002;
+
+Console.Write("Insira a senha para acessar: ");
+int senhaUsuario = int.Parse(Console.ReadLine());
+
+while (senhaUsuario != senha)
+{
+    Console.WriteLine("Senha invalida, tente novamente: ");
+    senhaUsuario = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Acesso liberado!");
