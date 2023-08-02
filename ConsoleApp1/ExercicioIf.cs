0517a14e88941aa6cf415290824547a15eb1f5c1
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-02 13:33:34 -0300

    Exercicio IF simples

---
+++ ConsoleApp1/ExercicioIf.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Que horas sao?");
+int horas = int.Parse(Console.ReadLine());
+if (horas < 12)
+{
+    Console.WriteLine("Bom dia!");
+}
+else if (horas < 18){
+    Console.WriteLine("Boa tarde!");
+}
+else
+{
+    Console.WriteLine("Boa noite!");
+}
---
+++ ExercicioIf.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Que horas sao?")
+int horas = int.Parse(Console.ReadLine());
+if (horas < 12)
+{
+    Consle.WriteLine("Bom dia!")
+}
+if else (horas < 18){
+    Consle.WriteLine("Boa tarde!")
+}
+else
+{
+    Consle.WriteLine("Boa noite!")
+}
