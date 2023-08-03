baed67fd49735558a8508f7c881e95e21d167623
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-03 13:57:46 -0300

    Branch master reorganizada e exercicios também

---
+++ teste/FOR.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= x; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
