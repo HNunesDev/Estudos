26585dfb0650c4003eb300310dcfd25708f115ed
Henrique Nunes <henriqueguilhem77@gmail.com>
2024-11-26 22:24:13 -0300

    Exercicio curso

---
+++ Exercicios/pt01.py
@@ -0,0 +1,9 @@
+print('Digite tres números: ')
+numero = input('Digite o primeiro número: ')
+numero2 = input('Digite o segundo número: ')
+numero3 = input('Digite o terceiro número: ')
+numero = int(numero*2)
+numero2 = int(numero2*2)
+numero3 = int(numero3*2)
+soma = numero + numero2 + numero3
+print(f'A soma dos números digitado foi: {soma}')
