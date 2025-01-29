ee84fb0b592460ab0371daab7b1bad8c973d2167
Henrique Nunes <henriqueguilhem77@gmail.com>
2025-01-29 20:34:17 -0300

    Desenvolvimento chatbot

---
+++ Exercicios/Conceitos/Sorted.py
@@ -0,0 +1,6 @@
+"""
+Não confundir sorted com sort, sort ordenad apenas listas
+O restante dos iteraveis incluindo listas é possivel usar sorted
+Sorted -> retorna sempre uma lista com o iteravel ordenado. Porem podemos converter o resultado
+Se utiizar reverse em seus parametros como True ele ordenada de maneira descrescente
+"""
