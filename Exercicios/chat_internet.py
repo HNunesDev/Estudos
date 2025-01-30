ca11a4862e6cdd0eb239f3e0cf6c3c491c811747
Henrique Nunes <henriqueguilhem77@gmail.com>
2025-01-30 11:24:51 -0300

    Criacao do bot coritnahns

---
+++ Exercicios/chat_internet.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import WebBaseLoader
+
+
+load_dotenv()
+os.environ['GROQ_API_KEY'] = os.getenv('api_key')
+chat = ChatGroq(model='llama-3.3-70b-versatile')
+
+loader = WebBaseLoader('https://pt.wikipedia.org/wiki/Sport_Club_Corinthians_Paulista')
+lista_documentos = loader.load()
+
+#Deve-se passar cada documento para uma string
+documentos = "\n".join([doc.page_content[:6000] for doc in lista_documentos])
+# print(documentos)
+
+template = ChatPromptTemplate.from_messages([
+    ("system", "Você é um assistente virtual amigável chamado Bmo e tem acesso às seguintes informações para dar suas respostas: {repertorio}"),
+    ("human", "{pergunta}")
+])
+pergunta_user = input('Qual a sua pergunta sobre o corinthans?')
+chain = template | chat
+resposta = chain.invoke({'repertorio': documentos, 'pergunta': pergunta_user})
+resposta2 = chain.invoke({'repertorio': documentos, 'pergunta': 'Se apresente, por favor?'})
+
+print(resposta.content)
+print(resposta2.content)
