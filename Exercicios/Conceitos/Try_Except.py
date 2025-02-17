634c36cb83fbe1efd0403647aa8c925c6e9f7b9f
Henrique Nunes <henriqueguilhem77@gmail.com>
2025-02-17 19:58:05 -0300

    Exemplo try_excpet

---
+++ Exercicios/Conceitos/Try_Except.py
@@ -0,0 +1,20 @@
+# É utilizado para tratar erro que podem surgir no código, previnindo que o programa pare de funcionar
+'''
+try:
+    Execucao do problema
+except:
+    Como seguir a partir do problema
+'''
+#Ex
+
+def pega_valor(dicionario, chave):
+    try:
+     return dicionario[chave]
+    except KeyError:
+       return None
+    except TypeError:
+       return None
+
+dic = {'olá': '23'}
+    
+print(pega_valor(1,'olá'))
