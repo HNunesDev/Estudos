604efd2b876f7fec5ca93ff5ce95452f19d9640c
Henrique Nunes <henriqueguilhem77@gmail.com>
2025-02-20 16:43:37 -0300

    Novo exemplo

--- Exercicios/Conceitos/Try_Except.py
+++ Exercicios/Conceitos/Try_Except.py
@@ -5,7 +5,7 @@ try:
 except:
     Como seguir a partir do problema
 '''
-#Ex
+#Ex 1
 
 def pega_valor(dicionario, chave):
     try:
@@ -16,5 +16,17 @@ def pega_valor(dicionario, chave):
        return None
 
 dic = {'olá': '23'}
-    
 print(pega_valor(1,'olá'))
+
+#EX 2
+def divisao(a,b):
+    try:
+        return int(a) / int(b)
+    except ValueError:
+        print('O valor precisa ser inteiro')
+    except ZeroDivisionError:
+        print('Impossivel realizar a divisao por 0')
+
+num1 = input('Passe o primeiro valor: ')
+num2 = input('Passe o segundo valor: ')
+print(divisao(num1, num2))
